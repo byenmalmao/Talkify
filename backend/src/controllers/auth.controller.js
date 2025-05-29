f5c7e06a618783b25b52df057e2d0fd4ecabe6e6
trancyenma <148397318+trancyenma@users.noreply.github.com>
2025-05-29 18:03:20 -0400

    subiedo

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export  async function signup (req, res){
+  res.send("Registrarse")    
+}
+
+export async function login (req, res){
+  res.send("Loguearse")    
+}
+
+export async function logout (req, res){
+  res.send("Desloguear")    
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.js";
+import { connectDB } from "./lib/db.js"
 
 dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT;
 
 
 
-app.get('/api/auth/signup', (req, res)=>{
-    res.send('HELLO WORD');
-})
-app.get('/api/auth/login', (req, res)=>{
-    res.send('HELLO WORD');
-})
-app.get('/api/auth/logout', (req, res)=>{
-    res.send('HELLO WORD');
-})
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () =>{
     console.log(`Esta corriendo el servidor en este puerto. ${PORT}`);
+    connectDB();
 })
