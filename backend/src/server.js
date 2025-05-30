1ecae5bb53e3f572ea3f7f1e8783ab8864bb1074
trancy <trancy@trancy.168.1.10>
2025-05-30 16:27:54 -0400

    Documentacion Server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,22 @@
 import express from "express";
-import dotenv from "dotenv";
-import authRoutes from "./routes/auth.js";
-import { connectDB } from "./lib/db.js"
+import dotenv from "dotenv"; //EXPORTA LA INSTRUCCION
+import authRoutes from "./routes/auth.js"; //Ruta para lo que son el proceso de autenticacion, loguearse, registrarse y desloguearse.
+import { connectDB } from "./lib/db.js"; //Ruta para la conexion de la base de datos
 
-dotenv.config();
-
-
-const app = express();
-const PORT = process.env.PORT;
+dotenv.config(); //Se cargar las variables de entornos, todas las variables que estan en el archivo .env, process.env.NOMBRE_ARIEBLE
 
 
+const app = express(); //Instacia EXPRESS, es decir aqui es el corazon del servidor de node.
+const PORT = process.env.PORT; //Variabel en .env
 
+
+
+//Es la URL DONDE SE VA CONSULTAR las rutas, 
 app.use("/api/auth", authRoutes);
 
+
+
+//Escucha el ppuerto donde el servidor se ejecutara
 app.listen(PORT, () =>{
     console.log(`Esta corriendo el servidor en este puerto. ${PORT}`);
     connectDB();
