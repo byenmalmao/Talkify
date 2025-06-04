c26ae1711939e1238b3a3f261657824a7029b0cf
byenmalmao <diamanteluz08@gmail.com>
2025-06-04 19:05:24 -0400

    Primer Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {Route,Routes} from "react-router";
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from  './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import OnboardingPage from './pages/OnboardingPage.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
+import ChatPage from './pages/ChatPage.jsx';
+import CallPage from './pages/CallPage.jsx';
+
+
 
 const App = () => {
   return (
-    <div class="bg-red-500 h-screen">App</div>
+    <div className="text-5xl h-screen" data-theme="night">
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/signup" element={<SignUpPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/onboarding" element={<OnboardingPage/>}/>
+        <Route path="notification" element={<NotificationsPage/>}/>
+        <Route path="chat" element={<ChatPage/>}/>
+        <Route path="call" element={<CallPage/>}/>
+      </Routes> 
+    </div>
+
   )
 }
 
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const  HomePage = () => {
+  return (
+    <div> <p>HOLA</p></div>
+  )
+}
+
+export default  HomePage;
