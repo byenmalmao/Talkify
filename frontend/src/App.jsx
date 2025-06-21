c80d15229b7df9f86cf25ae984b0546a535ce9de
byenmalmao <diamanteluz08@gmail.com>
2025-06-21 18:10:58 -0400

    Preview Onboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,28 +7,23 @@ import OnboardingPage from './pages/OnboardingPage.jsx';
 import NotificationsPage from './pages/NotificationsPage.jsx';
 import ChatPage from './pages/ChatPage.jsx';
 import CallPage from './pages/CallPage.jsx';
-
 import { Toaster } from "react-hot-toast";
 import { useEffect, useState } from "react";
+import PageLoader from './components/PageLoader.jsx';
+import useAuthUser from './hooks/useAuthUser.js';
 
-import { useQuery } from "@tanstack/react-query";
-import {axiosInstance} from "./lib/axios.js";
+
+
 const App = () => {
 
   //axios
 
-  //react query tanstack query ()
-  const { data: authData, isLoading, error } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
+ const {isLoading,authUser} = useAuthUser()
 
-      return res.data;
-    },
-    retry: false, //authcheck
-  });
+ const isAuthenticated = Boolean(authUser);
+ const isOnboarded = authUser?.isOnboarded;
 
-  const authUser = authData?.user
+  if(isLoading) return <PageLoader/>;
  
   return (
 
@@ -36,13 +31,16 @@ const App = () => {
       <Toaster />
 
       <Routes>
-        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage />: <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage />: <Navigate to="/" />} />
-        <Route path="/onboarding" element={authUser ? <OnboardingPage />: <Navigate to="/login" />} />
-        <Route path="/notification" element={authUser  ? <NotificationsPage />: <Navigate to="/login"/>} />
-        <Route path="/chat" element={authUser ? <ChatPage />: <Navigate to="/login"/>} />
-        <Route path="/call" element={authUser ? <CallPage />: <Navigate to="/login"/>} />
+        <Route path="/" element={isAuthenticated && isOnboarded? 
+            (<HomePage />) : 
+            ( <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />) 
+          } />
+        <Route path="/signup" element={!isAuthenticated ? <SignUpPage />: <Navigate to="/" />} />
+        <Route path="/login" element={!isAuthenticated ? <LoginPage />: <Navigate to="/" />} />
+        <Route path="/onboarding" element={isAuthenticated ? <OnboardingPage />: <Navigate to="/login" />} />
+        <Route path="/notification" element={isAuthenticated  ? <NotificationsPage />: <Navigate to="/login"/>} />
+        <Route path="/chat" element={isAuthenticated ? <ChatPage />: <Navigate to="/login"/>} />
+        <Route path="/call" element={isAuthenticated ? <CallPage />: <Navigate to="/login"/>} />
       </Routes>
     </div>
 
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { completeOnboarding, getAuthUser } from '../lib/api.js';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+
+
+import { CameraIcon , ShuffleIcon} from 'lucide-react'; // o el ícono que uses
+import useAuthUser from '../hooks/useAuthUser.js';
 
 const OnboardingPage = () => {
+
+  const {authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativelanguage: authUser?.nativelanguage || "",
+    learninglanguage: authUser?.learninglanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const {mutate: onboardingMutation, isPending} = useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess: () =>{
+      toast.success("Prodile onboarded successfully");
+      queryClient.invalidateQueries({queryKey: ["authUser"]});
+    }
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onboardingMutation(formState);
+  }
+
+  const handleRandomAvatar = () =>{};
   return (
-    <div>OnboardingPage</div>
+    <div className='min-h-screen bg-base-100 flex items-center justify-center p-4'>
+      <div className='card bg-base-200 w-full max-w-3xl shadow-xl'> 
+        <div className='card-body p-6 sm:p-8 '>
+          <h1 className='text-2xl sm:text-3xl font-bold text-center mb-6'>Complete your profile</h1>
+          <form onSubmit={handleSubmit} className='space-y-6'>
+
+            <div className='flex flex-col items-center justify-center space-y-4'>
+              <div className='size-32 rounded-full bg-base-300 overflow-hidden'>
+                {formState.profilePic? (
+                  <img 
+                  src={formState.profilePic}
+                  alt='Profile Preview'
+                  className='w-full h-full object-cover'
+                  />
+                ) : (
+                  <div className='flex items-center justify-center h-full'>
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )}
+
+              </div>
+
+              <div className='flex items center gap-2'>
+                <button className='btn btn-accent' type='button' onClick={handleRandomAvatar}>
+                  <ShuffleIcon className='size-4 mr-2'/>
+                  Generate Random Avatar
+                </button>
+              </div>
+
+            </div>
+            
+
+          </form>
+          
+        </div>
+
+      </div>
+    </div>
   )
 }
 
