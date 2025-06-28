d536f7cafccf2f1dadca826101a3946bb2e0a5a0
trancyenma <148397318+trancyenma@users.noreply.github.com>
2025-06-27 21:55:52 -0400

    Hostname

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,14 @@ import chatRoutes from "./routes/chat.route.js";
 import cors from "cors";
 import { connectDB } from "./lib/db.js"; //Ruta para la conexion de la base de datos
 import cookieParser from "cookie-parser"; //esto permite autenticar al usuario con el token almacenado en la cookie.
+
+import path from "path";
 dotenv.config(); //Se cargar las variables de entornos, todas las variables que estan en el archivo .env, process.env.NOMBRE_ARIEBLE
 
 
 const app = express(); //Instacia EXPRESS, es decir aqui es el corazon del servidor de node.
 const PORT = process.env.PORT; //Variabel en .env
-
+const __dirname = path.resolve();
 
 //Middleware
 app.use(cors({
@@ -26,6 +28,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) =>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+    })
+} ;
 
 
 //Escucha el ppuerto donde el servidor se ejecutara
